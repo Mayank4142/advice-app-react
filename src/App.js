abd9f438d92c77346b5f5d0083ff467405de0c3b
Mayank4142 <mayank.124142@gmail.com>
2024-08-30 21:21:19 +0530

    Loading message

--- src/App.js
+++ src/App.js
@@ -1,41 +1,45 @@
 import React from "react";
 import axios from "axios";
-
 import "./App.css";
 
 class App extends React.Component {
-    state = {advice : ""};
+    state = {
+        advice: "",
+        loading: false
+    };
 
     componentDidMount(){
         this.fetchAdvice();
     }
 
-    fetchAdvice = () => {
-        axios.get("https://api.adviceslip.com/advice")
-            .then((response) => {
-                const {advice} = response.data.slip;
- 
-                this.setState ({advice});
-            })
+    fetchAdvice = async () => {
+        const startTime = performance.now();
+        this.setState({ loading: true });
+        try {
+            const response = await axios.get("https://api.adviceslip.com/advice");
+            const { advice } = response.data.slip;
+            this.setState({ advice });
+        } catch (error) {
+            console.log(error);
+        } finally {
+            const endTime = performance.now();
+            console.log(`Data fetched and state updated in ${endTime - startTime}ms`);
+            this.setState({ loading: false });
+        }
+    };
 
-            .catch((error) => {
-                console.log(error);
-            })
-    }   
+    render() {
+        const { advice, loading } = this.state;
 
-    render(){
-        const { advice } = this.state; 
-
-        return(
+        return (
             <div className="app">
                 <div className="card">
-                    <h1 className="heading">{advice}</h1>
+                    <h1 className="heading">{loading ? "Loading..." : advice}</h1>
                     <button className="button" onClick={this.fetchAdvice}>
                         <span> GIVE ME ADVICE! </span>
                     </button>
                 </div>
             </div>
-            
         );
     }
 }
